530d42accee8478cb267eb3a54ee0c931176492e
Emma Wiggans <ea.wiggans@gmail.com>
2023-01-27 18:28:38 -0500

    got manager card to write to file

--- index.js
+++ index.js
@@ -4,10 +4,12 @@ const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
 const inquirer = require('inquirer');
 const writeFile = require('./src/generateHTML')
+const generateManager = require('./src/HTMLliterals')
+const fs = require('fs');
 let employeeArr = [];
 
 const promptMenu = () => {
-    return inquirer.prompt ([
+    return inquirer.prompt([
         {
             type: 'list',
             name: 'menu',
@@ -15,28 +17,26 @@ const promptMenu = () => {
             choices: ['Add manager', 'Add engineer', 'Add intern', 'Quit application']
         }
     ])
-    .then(choice => {
-        switch (choice.menu) {
-            case 'Add manager':
-                // manager prompts function here;
-                promptManager()
-                break;
-            case 'Add engineer':
-                // engineer prompts function here;
-                promptEngineer()
-                break;
-            case 'Add intern':
-                // intern prompts function here;
-                promptIntern()
-                break;
-            case 'Quit application':
-                // generate html function here;
-                const createTemp = (employeeArr) => {
-                    console.log("Writing html file")
-                }
-                break;
-        }
-    })
+        .then(choice => {
+            switch (choice.menu) {
+                case 'Add manager':
+                    // manager prompts function here;
+                    promptManager()
+                    break;
+                case 'Add engineer':
+                    // engineer prompts function here;
+                    promptEngineer()
+                    break;
+                case 'Add intern':
+                    // intern prompts function here;
+                    promptIntern()
+                    break;
+                case 'Quit application':
+                    // generate html function here;
+                    constructFile()
+                    break;
+            }
+        })
 }
 
 const promptManager = () => {
@@ -62,10 +62,13 @@ const promptManager = () => {
             name: 'officeNum',
         },
     ])
-    .then(function(answers) {
-        const managerData = new Manager(answers.name, answers.id, answers.email, answers.officeNum);
-        employeeArr.push(managerData);
-        return promptMenu();
+        .then(function (answers) {
+            const managerData = new Manager(answers.name, answers.id, answers.email, answers.officeNum);
+            employeeArr.push(managerData);
+            const managerCard = generateManager(managerData)
+            console.log(managerCard)
+            writeFile(managerCard)
+            return promptMenu();
         });
 };
 
@@ -92,10 +95,10 @@ const promptEngineer = () => {
             name: 'github',
         },
     ])
-    .then(function(answers) {
-        const engineerData = new Engineer(answers.name, answers.id, answers.email, answers.github);
-        employeeArr.push(engineerData);
-        return promptMenu();
+        .then(function (answers) {
+            const engineerData = new Engineer(answers.name, answers.id, answers.email, answers.github);
+            employeeArr.push(engineerData);
+            return promptMenu();
         });
 }
 
@@ -122,20 +125,31 @@ const promptIntern = () => {
             name: 'school',
         },
     ])
-    .then(function(answers) {
-        const internData = new Intern(answers.name, answers.id, answers.email, answers.school);
-        employeeArr.push(internData);
-        return promptMenu();
+        .then(function (answers) {
+            const internData = new Intern(answers.name, answers.id, answers.email, answers.school);
+            employeeArr.push(internData);
+            return promptMenu();
         });
 };
 
+
+// function constructFile () {
+//     fs.writeFile('./dist/index.html', generateManager(managerData), (err) =>{
+//         if (err) {
+//             console.log(err)
+//         }
+//         else {
+//             console.log("Team profile html generated successfully")
+//         }
+//     })
+// }
 const init = () => {
     promptMenu()
-    .then((employeeArr) => writeFile("./index.html", generateHTML(employeeArr), "UTF-8"))
-    .then(() => console.log("Successfully created team profile"))
-    .catch((err) => console.error(err))
 }
 
 init()
 
+
+
 // getGenMarkdown.generateHTML(employeeArr)))
+// writeFile("./dist/index.html", generateHTML(employeeArr), "UTF-8"))
